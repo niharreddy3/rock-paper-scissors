647d2725e215f1a458f61d8aea78c304e86ece20
Nihar Reddy <niharreddy3@gmail.com>
2024-07-17 14:56:30 -0500

    getHumanChoice and playRound functions completed.

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3);
 
@@ -13,4 +16,26 @@ function getComputerChoice() {
 function getHumanChoice() {
   let humanChoice = prompt("Select rock, paper or scissors.").toLowerCase();
   return humanChoice;
+}
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === "rock") {
+    if (computerChoice === "paper") {
+      computerScore += 1;
+    } else if (computerChoice === "scissor") {
+      humanScore += 1;
+    }
+  } else if (humanChoice === "paper") {
+    if (computerChoice === "scissors") {
+      computerScore += 1;
+    } else if (computerChoice === "rock") {
+      humanScore += 1;
+    }
+  } else if (humanChoice === "scissors") {
+    if (computerChoice === "rock") {
+      computerScore += 1;
+    } else if (computerChoice === "paper") {
+      humanScore += 1;
+    }
+  }
 }
